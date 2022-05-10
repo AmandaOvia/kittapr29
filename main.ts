fd7e0336b4e52b4c7956d0b6fe9b5154b1af0470
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-05-10 09:44:23 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,58 @@
-function green () {
-    range = strip.range(2, 1)
-    range.showColor(neopixel.colors(NeoPixelColors.Green))
+function Cross () {
     basic.pause(500)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(200)
+    for (let x = 0; x <= 9; x++) {
+        basic.showNumber(9 - x)
+    }
+    basic.clearScreen()
+}
+function green () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
 input.onButtonPressed(Button.A, function () {
-    RED()
-    basic.pause(1000)
-    green()
-    basic.pause(1000)
-    YELLOW()
-    basic.pause(1000)
+	
 })
 function RED () {
-    index = 20
-    basic.showIcon(IconNames.Yes)
-    for (let index = 0; index <= 4; index++) {
-        index = index - 1
-    }
     range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(5000)
+    range = strip.range(1, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function cannot_cross () {
+    basic.pause(100)
+    basic.showIcon(IconNames.No)
 }
 function YELLOW () {
-    range = strip.range(1, 1)
+    range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    basic.pause(500)
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let index = 0
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Angry)
-strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.setBrightness(255)
+basic.forever(function () {
+    RED()
+    basic.pause(1000)
+    Cross()
+    basic.pause(1000)
+    YELLOW()
+    basic.pause(1000)
+    green()
+    cannot_cross()
+    basic.pause(1000)
+})
